a639fd7ddc87fbb4c27793fe045bec34239fe3ca
fredvs <fiens@hotmail.com>
2020-08-29 21:42:51 +0200

    First commit.

---
+++ readme.md
@@ -0,0 +1,27 @@
+# MSEFileDialogX a extended MSEFileDialog.
+
+Here how to use it:
+
+Copy in the directory of your project: 
+
+- msefiledialogx.pas
+- msefiledialogx.mfm
+- msefiledialogx_mfm.pas
+
+Then add at end of uses section:
+
+uses
+...
+ , msefiledialogx;
+
+This will load the MSEFileDialogX each time a tfiledialog is called.
+
+Included a demo how to use it.
+
+![alt text](https://user-images.githubusercontent.com/3421249/91644861-60240000-ea40-11ea-99ea-f4eb723279aa.png)
+
+
+
+
+
+
---
+++ src/main.pas
@@ -0,0 +1,107 @@
+unit main;
+
+{$ifdef FPC}{$mode objfpc}{$h+}{$endif}
+interface
+
+uses
+  msetypes,
+  mseglob,
+  mseguiglob,
+  mseguiintf,
+  mseapplication,
+  msestat,
+  msemenus,
+  msegui,
+  msegraphics,
+  msegraphutils,
+  mseevent,
+  mseclasses,
+  msewidgets,
+  mseforms,
+  mseact,
+  msebitmap,
+  msedataedits,
+  msedatanodes,
+  msedragglob,
+  msedropdownlist,
+  mseedit,
+  msegrids,
+  msegridsglob,
+  mseificomp,
+  mseificompglob,
+  mseifiglob,
+  mselistbrowser,
+  msestatfile,
+  msestream,
+  msesys,
+  SysUtils,
+  msesimplewidgets,
+  msedispwidgets,
+  mserichstring,
+  msegraphedits,
+  msescrollbar,
+  msefiledialogx;
+
+{msefiledialogx in last position (or after msefiledialog) will override it}
+
+type
+  tmainfo = class(tmainform)
+    tfiledialog1: tfiledialog;
+    tbutton1: TButton;
+    tstringdisp1: tstringdisp;
+    tgroupbox1: tgroupbox;
+    b_fdk_open: tbooleaneditradio;
+    b_fdk_save: tbooleaneditradio;
+    b_fdk_dir: tbooleaneditradio;
+    b_fdk_none: tbooleaneditradio;
+    b_fdk_new: tbooleaneditradio;
+    thefilter: tstringedit;
+    thetitle: tstringedit;
+    custtitle: tbooleanedit;
+    thebasedir: tstringedit;
+    procedure onex(const Sender: TObject);
+  end;
+
+var
+  mainfo: tmainfo;
+
+implementation
+
+uses
+  main_mfm;
+
+procedure tmainfo.onex(const Sender: TObject);
+var
+  dialogkind: filedialogkindty;
+begin
+
+  if b_fdk_open.Value then
+    dialogkind := fdk_open
+  else if b_fdk_save.Value then
+    dialogkind := fdk_save
+  else if b_fdk_dir.Value then
+    dialogkind := fdk_dir
+  else if b_fdk_none.Value then
+    dialogkind := fdk_none
+  else if b_fdk_new.Value then
+    dialogkind := fdk_new;
+
+  tfiledialog1.controller.captionopen := 'Open File';
+  tfiledialog1.controller.captionsave := 'Save File';
+  tfiledialog1.controller.captiondir  := 'Open Directory';
+
+  tfiledialog1.controller.filter  := thefilter.Value;
+  tfiledialog1.controller.basedir := thebasedir.Value;
+
+  if custtitle.Value then
+  begin
+    if tfiledialog1.controller.Execute(dialogkind, thetitle.Value) = mr_ok then
+      tstringdisp1.Text := tfiledialog1.controller.filename;
+  end
+  else if tfiledialog1.controller.Execute(dialogkind) = mr_ok then
+    tstringdisp1.Text := tfiledialog1.controller.filename;
+
+end;
+
+end.
+
