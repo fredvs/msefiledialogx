535e5bf87a7566bf2ff4ea7663c8ec4a6691521f
fredvs <fiens@hotmail.com>
2020-09-03 16:23:54 +0200

    Added Color edits in demo and some tuning in layout.

--- src/main.pas
+++ src/main.pas
@@ -4,13 +4,45 @@ unit main;
 interface
 
 uses
- msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
- msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,mseact,
- msebitmap,msedataedits,msedatanodes,msedragglob,msedropdownlist,mseedit,
- msegrids,msegridsglob,mseificomp,mseificompglob,mseifiglob,mselistbrowser,
- msestatfile,msestream,msesys,SysUtils,msesimplewidgets,msedispwidgets,
- mserichstring,msegraphedits,msescrollbar,msefiledialogx, msesplitter,
- msecolordialog;
+  msetypes,
+  mseglob,
+  mseguiglob,
+  mseguiintf,
+  mseapplication,
+  msestat,
+  msemenus,
+  msegui,
+  msegraphics,
+  msegraphutils,
+  mseevent,
+  mseclasses,
+  msewidgets,
+  mseforms,
+  mseact,
+  msebitmap,
+  msedataedits,
+  msedatanodes,
+  msedragglob,
+  msedropdownlist,
+  mseedit,
+  msegrids,
+  msegridsglob,
+  mseificomp,
+  mseificompglob,
+  mseifiglob,
+  mselistbrowser,
+  msestatfile,
+  msestream,
+  msesys,
+  SysUtils,
+  msesimplewidgets,
+  msedispwidgets,
+  mserichstring,
+  msegraphedits,
+  msescrollbar,
+  msefiledialogx,
+  msesplitter,
+  msecolordialog;
 
 {msefiledialogx in last position (or after msefiledialog) will override it}
 
@@ -28,14 +60,14 @@ type
     thetitle: tstringedit;
     custtitle: tbooleanedit;
     thebasedir: tstringedit;
-   tfiledialog1: tfiledialog;
-   fontname: tdropdownlistedit;
-   fontheight: tintegeredit;
-   tbutton2: tbutton;
-   tbackcolor: tcoloredit;
-   tfontcolor: tcoloredit;
+    tfiledialog1: tfiledialog;
+    fontname: tdropdownlistedit;
+    fontheight: tintegeredit;
+    tbutton2: TButton;
+    tbackcolor: tcoloredit;
+    tfontcolor: tcoloredit;
     procedure onex(const Sender: TObject);
-   procedure onclose(const sender: TObject);
+    procedure onclose(const Sender: TObject);
   end;
 
 var
@@ -49,9 +81,8 @@ uses
 procedure tmainfo.onex(const Sender: TObject);
 var
   dialogkind: filedialogkindty;
-   ara, arb: msestringarty;
-
- begin
+  ara, arb: msestringarty;
+begin
   if b_fdk_open.Value then
     dialogkind := fdk_open
   else if b_fdk_save.Value then
@@ -66,37 +97,41 @@ var
   tfiledialog1.controller.captionopen := 'Open File';
   tfiledialog1.controller.captionsave := 'Save File as';
   tfiledialog1.controller.captiondir  := 'Open Directory';
-  
-    setlength(ara,5);
-    setlength(arb,5);
-    
-    ara[0] := 'Pascal';
-    ara[1] := 'C';
-    ara[2] := 'Java';
-    ara[3] := 'Python';
-    ara[4] := 'All';
-      
-    arb[0] := '"*.pp" "*.pas" "*.inc" "*.dpr" "*.lpr"';
-    arb[1] := '"*.c" "*.cpp" "*.h"';
-    arb[2] := '"*.java"';
-    arb[3] := '"*.py"';
-    arb[4] := '"*.*"';
-          
-   tfiledialog1.controller.filterlist.asarraya := ara;
-   tfiledialog1.controller.filterlist.asarrayb := arb;
-  
-   tfiledialog1.controller.filter  := thefilter.Value;
-  
-   tfiledialog1.controller.basedir := thebasedir.Value;
-  
-   tfiledialog1.controller.fontheight := fontheight.value; // font height of dialogfile
-   tfiledialog1.controller.fontname := fontname.value; // font name of dialogfile
-   if tfontcolor.value <> cl_none then
-   tfiledialog1.controller.fontcolor := tfontcolor.value; // font color of dialogfile
-   if tbackcolor.value <> cl_none then
-   tfiledialog1.controller.backcolor := tbackcolor.value; // background color of dialogfile
-    
-  
+
+  setlength(ara, 5);
+  setlength(arb, 5);
+
+  ara[0] := 'Pascal';
+  ara[1] := 'C';
+  ara[2] := 'Java';
+  ara[3] := 'Python';
+  ara[4] := 'All';
+
+  arb[0] := '"*.pp" "*.pas" "*.inc" "*.dpr" "*.lpr"';
+  arb[1] := '"*.c" "*.cpp" "*.h"';
+  arb[2] := '"*.java"';
+  arb[3] := '"*.py"';
+  arb[4] := '"*.*"';
+
+  tfiledialog1.controller.filterlist.asarraya := ara;
+  tfiledialog1.controller.filterlist.asarrayb := arb;
+
+  tfiledialog1.controller.filter := thefilter.Value;
+
+  tfiledialog1.controller.basedir := thebasedir.Value;
+
+  if fontheight.Value <> 0 then
+    tfiledialog1.controller.fontheight := fontheight.Value; // font height of dialogfile
+
+  if fontname.Value <> 'stf_default' then
+    tfiledialog1.controller.fontname := fontname.Value;     // font name of dialogfile
+
+  if tfontcolor.Value <> cl_none then
+    tfiledialog1.controller.fontcolor := tfontcolor.Value;  // font color of dialogfile
+
+  if tbackcolor.Value <> cl_none then
+    tfiledialog1.controller.backcolor := tbackcolor.Value;  // background color of dialogfile
+
   if custtitle.Value then
   begin
     if tfiledialog1.controller.Execute(dialogkind, thetitle.Value) = mr_ok then
@@ -107,9 +142,9 @@ var
 
 end;
 
-procedure tmainfo.onclose(const sender: TObject);
+procedure tmainfo.onclose(const Sender: TObject);
 begin
-close;
+  Close;
 end;
 
 end.
